6980e120ff0ee2e986582e431a5c609864ca561b
Maria Paulina Ramirez Vasquez <maria.ramirez@zinobe.com>
2024-08-12 20:11:33 -0500

    Add command for create initial user admin. Fix error on logout method.

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -39,7 +39,9 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()->currentAccessToken()->delete();
+        if($request->user()) {
+            $request->user()->currentAccessToken()->delete();
+        }
         return response()->json(['message' => 'Logged out successfully']);
     }
 
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Repositories\V1\UserRepository;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('create-initial-user', function() {
+    $password = $this->secret('Ingresa la contraseña');
+    if(!$password) {
+        return $this->comment('No ingresaste un password. Intenta nuevamente.');
+    }
+    $userRepository = app()->make(UserRepository::class);
+    $data = [
+        'email' => 'admin@marcgo.com',
+        'name' => 'Admin',
+        'password' => Hash::make($password)
+    ];
+    $userRepository->create($data);
+    $this->comment('Usuario creado!');
+});
